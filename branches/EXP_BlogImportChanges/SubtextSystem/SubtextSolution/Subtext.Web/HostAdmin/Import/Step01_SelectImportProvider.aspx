838787c6ca54827d2c0dc40f534e9816352bd646
ussherm <ussherm@903b54a8-190e-0410-ac3c-c9f2276e814e>
2005-10-13 02:20:14 +0000

    initial commit - Added the Import directory to the HostAdmin directory.  Also committing other Import changes.

---
+++ branches/EXP_BlogImportChanges/SubtextSystem/SubtextSolution/Subtext.Web/HostAdmin/Import/Step01_SelectImportProvider.aspx
@@ -0,0 +1,23 @@
+<%@ Register TagPrefix="MP" Namespace="Subtext.Web.Controls" Assembly="Subtext.Web.Controls" %>
+<%@ Page language="c#" Codebehind="Step01_SelectImportProvider.aspx.cs" AutoEventWireup="false" Inherits="Subtext.Web.HostAdmin.Step01_SelectImportProvider" %>
+<MP:MasterPage id="MPContainer" TemplateFile="~/HostAdmin/PageTemplate.ascx" runat="server">
+	<MP:ContentRegion id="MPTitle" runat="server">Subtext Import - Step 1 - Select an Import 
+Provider</MP:ContentRegion>
+	<MP:ContentRegion id="MPSubTitle" runat="server">Select an Import Provider</MP:ContentRegion>
+	<p>In this step, you’ll select an import provider used to import data from another 
+		blog engine. If you don’t see your blog engine listed here, then either you 
+		don’t have an import provider for that engine installed, or nobody has written 
+		one for it yet. Do I smell opportunity?
+	</p>
+	<H2>Import Providers</H2>
+	<p>
+		<asp:RadioButtonList id="rdlImportProviders" runat="server"></asp:RadioButtonList>
+		<asp:RequiredFieldValidator id="vldImportProviders" runat="server" 
+			ControlToValidate="rdlImportProviders" 
+			Display="Dynamic"
+			ErrorMessage="Please Select an Import Provider"></asp:RequiredFieldValidator>
+	</p>
+	<p>
+		<asp:Button id="btnNext" runat="server" text="Next - Gather Import Information"></asp:Button>
+	</p>
+</MP:MasterPage>
---
+++ branches/EXP_BlogImportChanges/SubtextSystem/SubtextSolution/Subtext.Web/HostAdmin/Import/Step01_SelectImportProvider.aspx.cs
@@ -0,0 +1,66 @@
+using System;
+using Subtext.Extensibility.Providers;
+
+namespace Subtext.Web.HostAdmin
+{
+	/// <summary>
+	/// Summary description for Step01_SelectImportProvider.
+	/// </summary>
+	public class Step01_SelectImportProvider : System.Web.UI.Page
+	{
+		protected Subtext.Web.Controls.ContentRegion MPTitle;
+		protected Subtext.Web.Controls.ContentRegion MPSubTitle;
+		protected System.Web.UI.WebControls.Button btnNext;
+		protected System.Web.UI.WebControls.RadioButtonList rdlImportProviders;
+		protected System.Web.UI.WebControls.RequiredFieldValidator vldImportProviders;
+		protected Subtext.Web.Controls.MasterPage MPContainer;
+	
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			if(!IsPostBack)
+			{
+				BindData();
+			}
+		}
+
+		void BindData()
+		{
+			this.rdlImportProviders.DataSource = ImportProvider.Providers.Values;
+			this.rdlImportProviders.DataTextField = "Description";
+			this.rdlImportProviders.DataValueField = "Name";
+			this.rdlImportProviders.DataBind();
+		}
+		
+		private void btnNext_Click(object sender, EventArgs e)
+		{
+			if(Page.IsValid)
+			{
+				Response.Redirect("Step02_GatherInfo.aspx?Provider=" + this.rdlImportProviders.SelectedValue);
+			}
+		}
+
+		#region Web Form Designer generated code
+		override protected void OnInit(EventArgs e)
+		{
+			//
+			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+			//
+			InitializeComponent();
+			base.OnInit(e);
+		}
+		
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+			this.Load += new System.EventHandler(this.Page_Load);
+
+		}
+		#endregion
+
+
+	}
+}
