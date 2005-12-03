863c360548df8911a79fc33cf76b47dc9441cf22
haacked <haacked@903b54a8-190e-0410-ac3c-c9f2276e814e>
2005-12-03 06:09:38 +0000

    Implemented a handler to generate BlogML output.  No need to save file on the server...

--- branches/EXP-BlogImportChanges_Refactor/SubtextSystem/SubtextSolution/Subtext.Web/Admin/ImportExport.aspx.cs
+++ branches/EXP-BlogImportChanges_Refactor/SubtextSystem/SubtextSolution/Subtext.Web/Admin/ImportExport.aspx.cs
@@ -23,15 +23,17 @@
 
 using System;
 using System.IO;
-using System.Web;
 using System.Web.UI.WebControls;
-using System.Xml;
-using Subtext.Framework.Configuration;
 using Subtext.Framework.Import;
 using Subtext.Web.Admin.WebUI;
 
 namespace Subtext.Web.Admin.Pages
 {
+	/// <summary>
+	/// Renders the page used to import and export blog data using 
+	/// the BlogML format proposed in 
+	/// <see href="http://markitup.com/Posts/PostsByCategory.aspx?categoryId=5751cee9-5b20-4db1-93bd-7e7c66208236">this blog</see>
+	/// </summary>
 	public class ImportExportPage : AdminPage
 	{
 		protected CheckBox chkDisplayOnScreen;
@@ -42,23 +44,11 @@ namespace Subtext.Web.Admin.Pages
 		protected AdvancedPanel Action;
 		protected MessagePanel Messages;
 
-		private string dirPath = HttpContext.Current.Server.MapPath("~/Admin/BlogML");
-		private string filePath = HttpContext.Current.Server.MapPath("~/Admin/BlogML/Data.xml");
 		protected System.Web.UI.WebControls.Button btnLoad;
 		protected System.Web.UI.HtmlControls.HtmlInputFile importBlogMLFile;
 	
 		private void Page_Load(object sender, EventArgs e)
 		{
-		}
-		
-		protected override void OnPreRender(EventArgs e) 
-		{
-			base.OnPreRender (e);
-
-			//	TODO: need to figure out a clever way to dynamically & randomly generate the file  
-			//	name for output and then remove them after they've been clicked or after it expires.
-			hypBlogMLFile.NavigateUrl = "~/Admin/BlogML/Data.xml";
-			hypBlogMLFile.Visible = File.Exists(filePath);
 		}
 
 		#region Web Form Designer generated code
@@ -86,45 +76,12 @@ namespace Subtext.Web.Admin.Pages
 
 		private void btnSave_Click(object sender, EventArgs e)
 		{
-			WriteBlogML();
+			Response.Redirect("~/Admin/Handlers/BlogMLExport.ashx?embed=" + this.chkEmbedAttach.Checked);
 		}
 
 		private void btnLoad_Click(object sender, System.EventArgs e)
 		{
 			LoadBlogML();
-		}
-
-		private void WriteBlogML()
-		{
-			string connStr = Config.Settings.ConnectionString;
-			SubtextBlogMLWriter blogWriter = new SubtextBlogMLWriter(connStr, Config.CurrentBlog.BlogID, false);
-			blogWriter.EmbedAttachments = this.chkEmbedAttach.Checked;
-
-			hypBlogMLFile.Visible = false;
-
-			if(!Directory.Exists(dirPath))
-			{
-				Directory.CreateDirectory(dirPath);
-			}
-			if(File.Exists(filePath))
-			{
-				File.Delete(filePath);
-			}
-
-			using(StreamWriter strWriter = File.CreateText(filePath))
-			{
-				XmlTextWriter xWriter = null;
-				try
-				{
-					xWriter = new XmlTextWriter(strWriter);
-					xWriter.Formatting = Formatting.Indented;
-					blogWriter.Write(xWriter);
-				}
-				finally
-				{
-					xWriter.Close();
-				}
-			}
 		}
 
 		private void LoadBlogML()
