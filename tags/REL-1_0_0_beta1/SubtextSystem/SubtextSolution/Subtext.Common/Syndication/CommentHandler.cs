d4bab92a140f618255b4ae533578db93e06dbf6a
nobody <nobody@903b54a8-190e-0410-ac3c-c9f2276e814e>
2006-02-25 00:31:52 +0000

    This commit was manufactured by cvs2svn to create tag 'REL-1_0_0_beta1'.

---
+++ tags/REL-1_0_0_beta1/SubtextSystem/SubtextSolution/Subtext.Common/Syndication/CommentHandler.cs
@@ -0,0 +1,83 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using System.Web;
+using System.Xml;
+using Subtext.Extensibility;
+using Subtext.Framework;
+using Subtext.Framework.Components;
+using Subtext.Framework.Format;
+using Subtext.Framework.Text;
+
+namespace Subtext.Common.Syndication
+{
+	// To Do: Should we validate the supplied XML? 
+	// Not much that can be done if it is not correct.
+
+	/// <summary>
+	/// Implementation of http://wellformedweb.org/story/9
+	/// Accepts a posted XML document via HttpPost.
+	/// </summary>
+	public class CommentHandler : IHttpHandler
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommentHandler"/> class.
+		/// </summary>
+		public CommentHandler()
+		{
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			HttpRequest Request = context.Request;
+			if(Request.RequestType == "POST" && Request.ContentType == "text/xml")
+			{
+
+				XmlDocument doc = new XmlDocument();
+				doc.Load(Request.InputStream);
+				Entry entry = new Entry(PostType.Comment);
+
+				string name = doc.SelectSingleNode("//item/author").InnerText;
+				if(name.IndexOf("<") != -1)
+				{
+					name = name.Substring(0,name.IndexOf("<"));
+				}
+                entry.Author = name.Trim();
+
+				entry.Body = doc.SelectSingleNode("//item/description").InnerText;
+			
+				entry.Title = doc.SelectSingleNode("//item/title").InnerText;
+				entry.TitleUrl = HtmlHelper.CheckForUrl(doc.SelectSingleNode("//item/link").InnerText);
+
+				entry.ParentID = UrlFormats.GetPostIDFromUrl(Request.Path);
+
+				Entries.InsertComment(entry);
+			}
+		}
+	
+		/// <summary>
+		/// Gets a value indicating whether another request can use
+		/// the <see cref="T:System.Web.IHttpHandler"/>
+		/// instance.
+		/// </summary>
+		/// <value></value>
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
+
---
+++ tags/REL-1_0_0_beta1/SubtextSystem/SubtextSolution/Subtext.Web/UI/Controls/MyLinks.cs
@@ -0,0 +1,70 @@
+using System;
+using Subtext.Framework;
+
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+namespace Subtext.Web.UI.Controls
+{
+	using System;
+
+	/// <summary>
+	///	Code behind class for the MyLinks section.
+	/// </summary>
+	public class MyLinks : BaseControl
+	{
+		protected System.Web.UI.WebControls.HyperLink Admin;
+		protected System.Web.UI.WebControls.HyperLink XMLLink;
+		protected System.Web.UI.WebControls.HyperLink AtomLink;
+		protected System.Web.UI.WebControls.HyperLink Syndication;
+		protected System.Web.UI.WebControls.HyperLink HomeLink;
+		protected System.Web.UI.WebControls.HyperLink ContactLink;
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad (e);
+			if(Context != null)
+			{			
+				if(HomeLink != null)
+				{
+					HomeLink.NavigateUrl = CurrentBlog.BlogHomeUrl;
+				}
+				if(ContactLink != null)
+				{
+					ContactLink.NavigateUrl = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}contact.aspx", CurrentBlog.RootUrl);
+				}
+
+				if(Request.IsAuthenticated && Security.IsAdmin)
+				{
+					Admin.Text = "Admin";
+					Admin.NavigateUrl = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}admin/default.aspx", CurrentBlog.RootUrl);
+				}
+				else
+				{
+					Admin.Text = "Login";
+					Admin.NavigateUrl = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}login.aspx", CurrentBlog.RootUrl);
+				}
+
+				Syndication.NavigateUrl = XMLLink.NavigateUrl = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}Rss.aspx", CurrentBlog.RootUrl);
+
+				if (AtomLink != null)
+					AtomLink.NavigateUrl = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}Atom.aspx", CurrentBlog.RootUrl);
+			}
+		}
+
+		
+	}
+}
+
