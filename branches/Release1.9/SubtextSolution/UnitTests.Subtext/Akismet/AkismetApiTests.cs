b4be9093ed0ca6f815e3e7215566b64d9d91e377
haacked <haacked@903b54a8-190e-0410-ac3c-c9f2276e814e>
2006-10-12 08:11:22 +0000

    Removed a test unit test.

--- branches/Release1.9/SubtextSolution/UnitTests.Subtext/Akismet/AkismetApiTests.cs
+++ branches/Release1.9/SubtextSolution/UnitTests.Subtext/Akismet/AkismetApiTests.cs
@@ -36,13 +36,6 @@ namespace UnitTests.Subtext.Akismet
 		#endregion
 
 		[Test]
-		public void CanParseIP()
-		{
-			IPAddress address = IPAddress.Parse("135.254.247.185, 192.11.54.1");
-			Assert.IsNotNull(address);
-		}
-		
-		[Test]
 		public void ConstructorSetsApiKeyAndUrl()
 		{
 			AkismetClient client = new AkismetClient("fake-key", new Uri("http://haacked.com/"), new HttpClient());
