39d0272edc7131875ec6a0f783ac60bf3507dc40
haacked <haacked@903b54a8-190e-0410-ac3c-c9f2276e814e>
2007-03-10 17:47:11 +0000

    Adding an installation file for the next version to make sure StoredProcs get updated.

---
+++ branches/Release1.9/SubtextSolution/Subtext.Installation/Scripts/Installation.01.09.05.sql
@@ -0,0 +1,5 @@
+/* Need to have this file so that we'll kick off the Auto-Upgrade logic 
+    because we need to update the Stored Procedures to fix some bugs
+    
+    - Fixing the blog creation proc.
+*/
