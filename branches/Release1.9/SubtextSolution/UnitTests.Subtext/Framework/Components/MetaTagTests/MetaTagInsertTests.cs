ae97a89adc7d92262d508cd4affb7c4511262c85
ussherm <ussherm@903b54a8-190e-0410-ac3c-c9f2276e814e>
2007-08-20 02:36:06 +0000

    fixing a few assertion messages

--- branches/Release1.9/SubtextSolution/UnitTests.Subtext/Framework/Components/MetaTagTests/MetaTagInsertTests.cs
+++ branches/Release1.9/SubtextSolution/UnitTests.Subtext/Framework/Components/MetaTagTests/MetaTagInsertTests.cs
@@ -17,7 +17,7 @@ namespace UnitTests.Subtext.Framework.Components.MetaTagTests
         [Row("no-cache", null, "cache-control", false, "Did not create blog specific MetaTag.")]
         [Row("Mon, 11 Jul 2020 11:12:01 GMT", null, "expires", true, "Did not create blog specific MetaTag.")]
         [Row(null, null, null, false, "MetaTag invalid - requires Contents and Name or Http-Equiv.", ExpectedException = typeof(ArgumentException))]
-        [Row(null, "author", null, false, "MetaTag invalid - requires Contents.", ExpectedException = typeof(ArgumentException))]
+        [Row(null, "author", null, true, "MetaTag invalid - requires Contents.", ExpectedException = typeof(ArgumentException))]
         [Row(null, null, "expires", false, "MetaTag invalid - requires Contents.", ExpectedException = typeof(ArgumentException))]
         [Row("sending nulls", null, null, false, "MetaTag invalid - requires Name or Http-Equiv.", ExpectedException = typeof(ArgumentException))]
         [Row("set both attributes", "description", "expires", true, "MetaTag invalid - requires either Name or Http-Equiv.", ExpectedException = typeof(ArgumentException))]
@@ -42,18 +42,18 @@ namespace UnitTests.Subtext.Framework.Components.MetaTagTests
 
             tags = MetaTags.GetMetaTagsForBlog(blog);
 
-            Assert.AreEqual(1, tags.Count);
+            Assert.AreEqual(1, tags.Count, errMsg);
 
             MetaTag newTag = tags[0];
 
             // make sure all attributes of the meta-tag were written to the data store correctly.
-            Assert.AreEqual(tagId, newTag.Id);
-            Assert.AreEqual(mt.Content, newTag.Content);
-            Assert.AreEqual(mt.Name, newTag.Name);
-            Assert.AreEqual(mt.HttpEquiv, newTag.HttpEquiv);
-            Assert.AreEqual(mt.BlogId, newTag.BlogId);
-            Assert.AreEqual(mt.EntryId, newTag.EntryId);
-            Assert.AreEqual(mt.DateCreated.Date, newTag.DateCreated.Date);
+            Assert.AreEqual(tagId, newTag.Id, "Wrong Id");
+            Assert.AreEqual(mt.Content, newTag.Content, "Wrong content");
+            Assert.AreEqual(mt.Name, newTag.Name, "wrong name attribute");
+            Assert.AreEqual(mt.HttpEquiv, newTag.HttpEquiv, "Wrong http-equiv attriubte");
+            Assert.AreEqual(mt.BlogId, newTag.BlogId, "Wrong blogId");
+            Assert.AreEqual(mt.EntryId, newTag.EntryId, "Wrong entryId");
+            Assert.AreEqual(mt.DateCreated.Date, newTag.DateCreated.Date, "Wrong created date");
         }
 
         [SetUp]
