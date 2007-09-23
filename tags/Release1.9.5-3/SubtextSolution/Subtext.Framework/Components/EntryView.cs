ffadc5368b191c0ae5ffb540a89a18f633d3ffbe
haacked <haacked@903b54a8-190e-0410-ac3c-c9f2276e814e>
2007-09-23 06:08:08 +0000

    made a copy

---
+++ tags/Release1.9.5-3/SubtextSolution/Subtext.Framework/Components/EntryView.cs
@@ -0,0 +1,64 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+
+namespace Subtext.Framework.Components
+{
+	/// <summary>
+	/// Summary description for EntryView.
+	/// </summary>
+	[Serializable]
+	public class EntryView
+	{
+		public EntryView()
+		{
+		}
+
+		private int _blogID;
+		public int BlogId
+		{
+			get {return this._blogID;}
+			set {this._blogID = value;}
+		}
+
+		private int _entryID;
+		public int EntryID
+		{
+			get {return this._entryID;}
+			set {this._entryID = value;}
+		}
+
+		private string _referralUrl;
+		public string ReferralUrl
+		{
+			get {return this._referralUrl;}
+			set {this._referralUrl = value;}
+		}
+
+		private PageViewType _pageViewType;
+		public PageViewType PageViewType
+		{
+			get {return this._pageViewType;}
+			set {this._pageViewType = value;}
+		}
+	}
+
+	public enum PageViewType : byte
+	{
+		AggView = 0,
+		WebView = 1
+	}
+}
