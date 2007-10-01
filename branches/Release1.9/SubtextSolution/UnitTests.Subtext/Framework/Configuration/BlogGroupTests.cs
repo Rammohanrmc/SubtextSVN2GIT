212f4a51e353547b5e3b502db23592c593b867bd
haacked <haacked@903b54a8-190e-0410-ac3c-c9f2276e814e>
2007-10-01 23:30:58 +0000

    Forgot to add this file.

---
+++ branches/Release1.9/SubtextSolution/UnitTests.Subtext/Framework/Configuration/BlogGroupTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+
+namespace UnitTests.Subtext.Framework.Configuration
+{
+	[TestFixture]
+	public class BlogGroupTests
+	{
+		[Test]
+		[RollBack2]
+		public void CanListBlogGroups()
+		{
+
+		}
+	}
+}
